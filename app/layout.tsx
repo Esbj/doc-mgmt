a9a9ab5ee67e7aaf36b9a4ecf73afd00b5144235
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-10-27 12:17:02 +0200

    un-nessisairy import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import 'material-icons/iconfont/material-icons.css';
-import Link from 'next/link';
 import Header from '@/components/Header';
 const inter = Inter({ subsets: ['latin'] })
 
