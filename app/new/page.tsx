88256b62821f3339aa6e750389829b36f5451029
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-10-28 14:45:39 +0200

    added color to tinymc

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -7,13 +7,14 @@ export default function NewDoc() {
 
 
   return (
-    <div className='container mx-auto w-3/4'>
-      <input className='rounded-md w-fit mx-auto text-2xl p-2' type="text" name="Heading" id="heading" placeholder='Document title' />
+    <div className='mx-auto flex flex-col gap-6 mt-6 w-3/4'>
+      <input className='p-2 text-2xl w-1/2 mx-auto rounded-md ' type="text" name="heading" id="heading" placeholder='Title' />
       <Editor
         apiKey='vk5q0d6twde7ii4ehncmcrkz6c0mls0s0kpn7re6kqa8grr8'
         init={{
-          plugins: ' emoticons  link lists searchreplace visualblocks wordcount checklist casechange export formatpainter pageembed permanentpen footnotes advtemplate tableofcontents mergetags powerpaste tinymcespellchecker autocorrect typography inlinecss',
-          toolbar: 'undo redo | blocks fontsize | bold italic underline strikethrough | link mergetags | align | checklist numlist bullist| emoticons ',
+          menubar: false,
+          plugins: ' textcolor  link lists checklist casechange export formatpainter pageembed permanentpen footnotes advtemplate tableofcontents mergetags powerpaste tinymcespellchecker autocorrect typography inlinecss',
+          toolbar: ' undo redo | forecolor backcolor | blocks fontsize | bold italic underline strikethrough | link mergetags | align | checklist numlist bullist ',
         }}
       />
 
