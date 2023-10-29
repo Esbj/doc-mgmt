e052e91ac294b04c91482a33fe0a7b455ecf9ad8
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-10-29 15:46:41 +0100

    editor working

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -4,20 +4,23 @@ import EditorComponent from '@/components/EditorComponent';
 import React, { useEffect, useState } from 'react'
 
 export default function Edit({ params }: { params: { id: number } }) {
+  const [doc, setDoc] = useState();
+
   const handleSave = (title: string, content: string) => {
+    console.log(title, content)
     //fetch insert endpoint
   }
-  const [doc, setDoc] = useState()
   useEffect(() => {
     const getDoc = async () => {
       const result = await fetch(`/api/${params.id}/`);
-      const docFromApi = await result.json();
-      console.log(docFromApi)
-      setDoc(docFromApi)
+      const parsedDoc = await result.json();
+      const response = parsedDoc[0]
+      console.log(response)
+      setDoc(response)
     };
     getDoc();
+  }, [])
 
-  }, [params.id])
 
   return (
     <EditorComponent onSave={handleSave} initialContent={doc} />
--- components/EditorComponent.tsx
+++ components/EditorComponent.tsx
@@ -2,7 +2,7 @@
 import { Doc } from '@/lib/interface'
 import { Editor } from '@tinymce/tinymce-react'
 import { init } from 'next/dist/compiled/webpack/webpack'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
   onSave: (title: string, content: any) => void
@@ -10,17 +10,17 @@ type Props = {
 }
 
 export default function EditorComponent({ onSave, initialContent }: Props) {
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState(initialContent?.title || "");
+  const [content, setContent] = useState(initialContent?.content || "")
 
-  if (initialContent?.content && initialContent.title) {
-    setTitle(initialContent.title);
-    setContent(initialContent.content);
-  }
+  useEffect(() => {
+    setContent(initialContent?.content || "");
+    setTitle(initialContent?.title || "")
+  }, [initialContent])
 
   return (
     <div className='mx-auto flex flex-col gap-6 mt-6 w-3/4'>
-      <input className='p-2 text-2xl w-1/2 mx-auto rounded-md ' type="text" name="heading" id="heading" placeholder='Title' onChange={e => setTitle(e.target.value)} value={title} />
+      <input className='p-2 text-2xl w-1/2 mx-auto rounded-md ' type="text" name="heading" id="heading" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} />
       <Editor
         apiKey='vk5q0d6twde7ii4ehncmcrkz6c0mls0s0kpn7re6kqa8grr8'
         value={content}
